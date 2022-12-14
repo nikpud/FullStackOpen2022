7a53c344ef036e564c21fdf9c70b8140df0810c4
Nik Pudaruth <nik.pudaruth@gmail.com>
2022-12-14 19:01:55 +1100

    created separate component Hello

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,27 @@
-const app = () => (
-  <div>
-    <p>Full Stack Open part1</p>
-  </div>
-)
+const Hello = () => {
 
-export default app
+  const now = new Date()
+
+  return (
+    <p>It is {now.toString()}</p>
+
+  )
+}
+
+const App = () => {
+
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <Hello />
+      <p>
+        {a} multiplied by {b} = {a * b}
+      </p>
+      <p>Full Stack Open part1</p>
+    </div>
+  )
+}
+
+export default App
